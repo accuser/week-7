01b6e3def2d4efd0e508319c0703ec5e1211fbb1
Matthew Gibbons <matthew.gibbons@multiverse.io>
2022-11-16 15:24:13 +0000

    Bump

--- src/models/person.js
+++ src/models/person.js
@@ -1,23 +1,45 @@
+/**
+ * A person with a first and last name.
+ */
 class Person {
   #firstName;
   #lastName;
 
+  /**
+   * Construct a new person instance.
+   *
+   * @param {string} firstName
+   * @param {string} lastName
+   */
   constructor(firstName, lastName) {
-    this.#firstName = firstName;
-    this.#lastName = lastName;
+    if (firstName && typeof firstName === "string") {
+      this.#firstName = firstName;
+    } else {
+      throw new TypeError("Invalid firstName");
+    }
+
+    if (lastName && typeof lastName === "string") {
+      this.#lastName = lastName;
+    } else {
+      throw new TypeError("Invalid lastName");
+    }
   }
 
   get firstName() {
     return this.#firstName;
   }
 
+  get fullName() {
+    return [this.firstName, this.lastName].join(" ");
+  }
+
   get lastName() {
     return this.#lastName;
   }
+
+  toString() {
+    return `Person { firstName: "${this.firstName}", fullName: "${this.fullName}", lastName: "${this.lastName}" }`;
+  }
 }
-
-const alice = new Person("Alice", "Smith");
-
-console.log(alice);
 
 module.exports = Person;
--- test/person.test.js
+++ test/person.test.js
@@ -1,11 +1,50 @@
-const Person = require("../src/models/person");
+const Person = require("../src/person");
 
 describe("Person", () => {
   describe("constructor", () => {
-    it("creates an instance of Person", () => {
-      const person = new Person("Olive", "Rudd");
+    describe("with valid arguments", () => {
+      it("creates an instance of Person", () => {
+        const person = new Person("Olive", "Rudd");
 
-      expect(person).toBeInstanceOf(Person);
+        expect(person).toBeInstanceOf(Person);
+      });
+    });
+
+    describe("with no arguments", () => {
+      it("throws a TypeError", () => {
+        expect(() => new Person()).toThrow(TypeError);
+        expect(() => new Person()).toThrow("Invalid firstName");
+      });
+    });
+
+    describe("with no lastName argument", () => {
+      it("throws a TypeError", () => {
+        expect(() => new Person("Olive")).toThrow("Invalid lastName");
+      });
+    });
+
+    describe("with an empty firstName argument", () => {
+      it("throws a TypeError", () => {
+        expect(() => new Person("", "Rudd")).toThrow("Invalid firstName");
+      });
+    });
+
+    describe("with an empty lastName argument", () => {
+      it("throws a TypeError", () => {
+        expect(() => new Person("Olive", "")).toThrow("Invalid lastName");
+      });
+    });
+
+    describe("with a non-string firstName argument", () => {
+      it("throws a TypeError", () => {
+        expect(() => new Person(42, "Rudd")).toThrow("Invalid firstName");
+      });
+    });
+
+    describe("with a non-string lastName argument", () => {
+      it("throws a TypeError", () => {
+        expect(() => new Person("Olive", 42)).toThrow("Invalid lastName");
+      });
     });
   });
 
@@ -35,6 +74,35 @@ describe("Person", () => {
       const { firstName } = person;
 
       expect(firstName).toBe("Olive");
+    });
+  });
+
+  it("has a fullName property", () => {
+    const person = new Person("Olive", "Rudd");
+
+    expect(person).toHaveProperty("fullName");
+  });
+
+  describe("fullName", () => {
+    it("is a string", () => {
+      const { fullName } = new Person("Olive", "Rudd");
+
+      expect(typeof fullName).toBe("string");
+    });
+
+    it("is derrived from the person's first and last name", () => {
+      const { fullName } = new Person("Olive", "Rudd");
+
+      expect(fullName).toBe("Olive Rudd");
+    });
+
+    it("is readonly", () => {
+      const person = new Person("Olive", "Rudd");
+      person.fullName = "Olive Brockwell";
+
+      const { fullName } = person;
+
+      expect(fullName).toBe("Olive Rudd");
     });
   });
 
